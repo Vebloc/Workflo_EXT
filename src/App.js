50ac5ed1b412139935f7282898c2d353dca1f130
OsamaAlhaq <osama_alhaq@hotmail.com>
2023-12-07 10:50:06 -0500

    Setup repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import OnboardingMain from './components/pages/OnboardingMain.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OnboardingMain />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/OnboardingMain.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const OnbordingMain = (props) => {
+
+  let name = 'My Name';
+
+  return (
+    <div className="d-flex" style={{ width: '100vw', height: '100vh' }}>
+      <div
+        className="d-flex left-panel">
+        <h3 style={{
+          fontSize: '35px',
+        }}>
+          {`Welcome ${name}, Name your first project:`}
+        </h3>
+        <input
+          onChange={e => {
+            console.info(e?.current?.value)
+          }}
+          className='left-input'
+          type='text'
+          placeholder={`${name}'s Workspace`}
+        />
+        <button
+          className="butn-primary"
+          onClick={() => {
+            console.info('Clicked')
+          }}>
+          Continue
+        </button>
+      </div>
+      <div className='right-panel'>
+
+      </div>
+    </div>
+  );
+};
+
+export default OnbordingMain;
+
